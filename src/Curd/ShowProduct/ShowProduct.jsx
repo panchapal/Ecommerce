a4393cbd51c9f88e90a5d55ecf53f4bf66c13de2
Panchanan Pal <you@wxample.com>
2024-09-04 09:45:41 +0530

    co

--- src/Curd/ShowProduct/ShowProduct.jsx
+++ src/Curd/ShowProduct/ShowProduct.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { product_get, product_delete } from "../../Redux/cartSlice";
 import {
-  Button,
+  // Button,
   Card,
   CardContent,
   CardMedia,
